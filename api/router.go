3c97990a5c1ae1a802aee1bda1e33e430bf55597
JohnnyKahiu <wawerukahiu@gmail.com>
2025-11-15 21:04:15 +0300

    added api routes

--- api/router.go
+++ api/router.go
@@ -1,5 +1,30 @@
 package api
 
-func NewRouter() {
+import (
+	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
+)
+
+var mySigningKey = []byte(os.Getenv("SPEEDSALESJWTKEY"))
+
+func EnableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
+}
+
+func NewRouter() *mux.Router {
+	// rentals.CreateTables()
+
+	r := mux.NewRouter()
+
+	// r.HandleFunc("/ws", socketHandler)
+
+	r.HandleFunc("/products/search/{module}", SearchGet).Methods("GET", "OPTIONS")
+
+	// r.HandleFunc("/sms", sms.Post).Methods("POST", "OPTIONS")
+
+	return r
 }
---
+++ internal/search/products.go
@@ -0,0 +1,91 @@
+package search
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/JohnnyKahiu/speedsales_inventory/pkg/products"
+	"github.com/gorilla/mux"
+)
+
+func GetRoutes(w http.ResponseWriter, r *http.Request) map[string]interface{} {
+	respMap := make(map[string]interface{})
+
+	vars := mux.Vars(r)
+
+	m := vars["module"]
+
+	if m == "code" {
+		fmt.Println("searching by code")
+		key := r.URL.Query().Get("key")
+
+		// get values from database
+		value, err := products.GetByCode(key, false)
+		if err != nil {
+			respMap["response"] = "error"
+			respMap["message"] = "failed to get_by_code"
+			respMap["trace"] = err
+			return respMap
+		}
+
+		respMap["response"] = "success"
+		respMap["values"] = value
+		return respMap
+	}
+
+	if m == "name" {
+		key := r.URL.Query().Get("key")
+
+		// get values from database
+		values, err := products.SearchDescription(key)
+		if err != nil {
+			respMap["response"] = "error"
+			respMap["message"] = "failed searching"
+			respMap["trace"] = err
+			return respMap
+		}
+
+		respMap["response"] = "success"
+		respMap["values"] = values
+		return respMap
+	}
+	if m == "category" {
+		key := r.URL.Query().Get("key")
+
+		vals, err := products.SearchByCategory(key)
+		if err != nil {
+			respMap["response"] = "error"
+			respMap["message"] = "failed to fetch categories"
+			respMap["trace"] = err
+			return respMap
+		}
+
+		respMap["response"] = "success"
+		respMap["values"] = vals
+		return respMap
+	}
+	if m == "all" {
+		l := r.URL.Query().Get("limit")
+		if l == "" {
+			l = "100"
+		}
+
+		limit, _ := strconv.ParseInt(l, 10, 0)
+		vals, err := products.All(int(limit))
+		if err != nil {
+			respMap["response"] = "error"
+			respMap["message"] = "failed to fetch categories"
+			respMap["trace"] = err
+			return respMap
+		}
+
+		respMap["response"] = "success"
+		respMap["values"] = vals
+		return respMap
+	}
+
+	respMap["response"] = "error"
+	respMap["message"] = "unresolved path"
+	return respMap
+}
